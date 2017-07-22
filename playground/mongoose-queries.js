2e4be8dd70b807e5d5a8916d7113d350246bc4a7
Ivan Denardi <ivan.denardi@gmail.com>
2017-07-21 23:22:47 -0300

    Add queries playground

---
+++ playground/mongoose-queries.js
@@ -0,0 +1,32 @@
+const {ObjectID} = require('mongodb');
+
+const {mongoose} = require('./../server/db/mongoose');
+const {Todo} = require('./../server/models/todo');
+
+var id = '5972afba6ba767196fb51ad61';
+
+if(!ObjectID.isValid(id)) {
+    console.log('Id not valid');
+}
+
+// Todo.find({
+//     _id: id
+// }).then((todos) => {
+//     console.log('Todos', todos);
+// });
+
+// Todo.findOne({
+//     _id: id
+// }).then((todo) => {
+//     if(!todo) {
+//         return console.log('Id not found')
+//     }
+//     console.log('Todo', todo);
+// });
+
+Todo.findById(id).then((todo) => {
+    if(!todo) {
+        return console.log('Id not found')
+    }
+    console.log('Todo by id', todo);
+}).catch((e) => console.log(e));
